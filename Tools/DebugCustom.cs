c3d8c89b12e06116baf3ca9e74309205c88ff266
NhuTuyen <tuyen.teras@gmail.com>
2025-11-03 16:29:47 +0700

    add DebugCustom

---
+++ Tools/DebugCustom.cs
@@ -0,0 +1,174 @@
+using Debug = UnityEngine.Debug;
+using Newtonsoft.Json;
+public class DebugCustom
+{
+    private static bool _isLogBug = true;
+    private static bool IsLogBug
+    {
+        get
+        {
+            if (!_isLogBug)
+                return false;
+            return true;
+        }
+    }
+    public static void LogWarning(params object[] content)
+    {
+#if UNITY_EDITOR
+        if (!IsLogBug)
+            return;
+        string str = "";
+        for (int i = 0; i < content.Length; i++)
+        {
+            if (i == content.Length - 1)
+            {
+                str += content[i].ToString();
+            }
+            else
+            {
+                str += content[i].ToString() + "";
+            }
+        }
+        Debug.LogWarning(str);
+#endif
+    }
+    public static void Log(params object[] content)
+    {
+#if UNITY_EDITOR
+        if (!IsLogBug)
+            return;
+        string str = "";
+        for (int i = 0; i < content.Length; i++)
+        {
+            if (i == content.Length - 1)
+            {
+                str += content[i].ToString();
+            }
+            else
+            {
+                str += content[i].ToString() + "";
+            }
+        }
+        Debug.Log(str);
+#endif
+    }
+#if UNITY_EDITOR
+    public static string ReturnLog(params object[] content)
+    {
+        string str = "";
+        for (int i = 0; i < content.Length; i++)
+        {
+            if (i == content.Length - 1)
+            {
+                str += content[i].ToString();
+            }
+            else
+            {
+                str += content[i].ToString() + "";
+            }
+        }
+        Debug.Log(str);
+        return str;
+    }
+#endif
+    public static void LogError(params object[] content)
+    {
+#if UNITY_EDITOR
+        if (!IsLogBug)
+            return;
+        string str = "";
+        for (int i = 0; i < content.Length; i++)
+        {
+            if (i == content.Length - 1)
+            {
+                str += content[i].ToString();
+            }
+            else
+            {
+                str += content[i].ToString() + "";
+            }
+        }
+        Debug.LogError(str);
+#endif
+    }
+    public static void LogColor(params object[] content)
+    {
+#if UNITY_EDITOR || GAME_ROCKET
+        if (!IsLogBug)
+            return;
+        string str = "";
+        for (int i = 0; i < content.Length; i++)
+        {
+            if (i == content.Length - 1)
+            {
+                str += content[i].ToString();
+            }
+            else
+            {
+                str += content[i].ToString() + "";
+            }
+        }
+        Debug.Log("<color=\"" + "#ffa500ff" + "\">" + str + "</color>");
+#endif
+    }
+    public static void LogColorJson(params object[] content)
+    {
+#if UNITY_EDITOR
+        if (!IsLogBug)
+            return;
+        string str = "";
+        for (int i = 0; i < content.Length; i++)
+        {
+            if (i == content.Length - 1)
+            {
+                str += JsonConvert.SerializeObject(content[i]);
+            }
+            else
+            {
+                str += JsonConvert.SerializeObject(content[i]) + "";
+            }
+        }
+        Debug.Log("<color=\"" + "#ffa500ff" + "\">" + "count:" + content.Length + "data" + str + "</color>");
+#endif
+    }
+    public static void LogJson(params object[] content)
+    {
+#if UNITY_EDITOR
+        if (!IsLogBug)
+            return;
+        string str = "";
+        for (int i = 0; i < content.Length; i++)
+        {
+            if (i == content.Length - 1)
+            {
+                str += JsonConvert.SerializeObject(content[i]);
+            }
+            else
+            {
+                str += JsonConvert.SerializeObject(content[i]) + "";
+            }
+        }
+        Debug.Log(str);
+#endif
+    }
+    public static void LogErrorJson(params object[] content)
+    {
+#if UNITY_EDITOR
+        if (!IsLogBug)
+            return;
+        string str = "";
+        for (int i = 0; i < content.Length; i++)
+        {
+            if (i == content.Length - 1)
+            {
+                str += JsonConvert.SerializeObject(content[i]);
+            }
+            else
+            {
+                str += JsonConvert.SerializeObject(content[i]) + "";
+            }
+        }
+        Debug.LogError(str);
+#endif
+    }
+}
